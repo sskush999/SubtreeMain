bab985aec7e414fafc24dd993068e83020f17fc7
Shubham Kumar Singh <shubham.singh@IXEDSJRMAC0025.local>
2021-02-11 11:33:58 +0530

    Initial commit

---
+++ Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  ScreenDemo
+//
+//  Created by Shubham Kumar Singh on 02/02/21.
+//
+
+import Foundation
+
+class Constant {
+    
+    static let sdkBundle = Bundle(for: Constant.self)
+    static let username = "usernameKey"
+    static let password = "passwordKey"
+    static let login = "loginKey"
+}
